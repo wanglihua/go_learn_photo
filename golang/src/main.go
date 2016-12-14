56a53fd7f24b31bef1960d330d2a0712dbc08f09
Richard.Wang <Richard.Wang@linkores.com>
2016-12-14 14:04:43 +0800

    go

--- golang/src/main.go
+++ golang/src/main.go
@@ -2,29 +2,16 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"sync"
 )
 
-//ToDo: ddd
 func main() {
-	var str string = "Hello, 你好"
-	for i := 0; i < len(str); i++ {
-		fmt.Println(str[i])
-	}
-
 	fmt.Println()
 
-	for _, c := range str {
-		fmt.Println(string(c))
-	}
-
-	fmt.Println(os.Getenv("HOME"))
-
-	say_hello()
-
-	print("ddd")
+	var once sync.Once
+	once.Do(Test)
 }
 
-func say_hello() {
-	fmt.Println("Hello world!")
+func Test() {
+	fmt.Println("test")
 }
